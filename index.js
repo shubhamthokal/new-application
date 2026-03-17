cc9fab66e408fffd976a38a0fe076ec80ede44bc
jayesh-pixel <jayeshpandey11789@gmail.com>
2026-03-17 14:49:30 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8090;
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
